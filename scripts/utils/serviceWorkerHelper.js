fcbb806c9c8dd2eb9fb4ce972f1b49d9241eccbe
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-22 20:59:21 +0100

    Create serviceWorkerHelper.js

---
+++ scripts/utils/serviceWorkerHelper.js
@@ -0,0 +1,10 @@
+
+
+export function registerSW() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('Service Worker registered'))
+      .catch(err => console.error('SW registration failed:', err));
+  }
+}
+
